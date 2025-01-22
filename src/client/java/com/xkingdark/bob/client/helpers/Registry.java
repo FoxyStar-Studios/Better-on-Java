77ca57a9fd023e4e9ce269a0a97c7d0b1f0ccd79
XxPoggyisLitxX <99201951+XxPoggyisLitxX@users.noreply.github.com>
2025-01-23 00:12:32 +0200

    Update Registry.java

--- src/client/java/com/xkingdark/bob/client/helpers/Registry.java
+++ src/client/java/com/xkingdark/bob/client/helpers/Registry.java
@@ -36,5 +36,7 @@ public class Registry {
 
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.PINK_LAVENDER, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.TALL_LAVENDER, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.BLUEGROD, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.TALLER_GRASS, RenderLayer.getCutout());
     };
 };
